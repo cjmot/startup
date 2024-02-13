a46de770c9ec5f62a9e1ad7b75bb317150dd7e10
Cooper Motyer <cjmotyer@gmail.com>
2024-02-13 16:09:58 -0700

    put in tailwind cdn instead of using local files, shop.html style updated

--- startup-html/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["../startup-html/**/*.html", "../startup-html/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
