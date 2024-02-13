ca631a7e3c65f9cccee73d00e207872cbbc08e89
Cooper Motyer <cjmotyer@gmail.com>
2024-02-12 19:48:29 -0700

    Added tailwind, formatted login page(index.html)

---
+++ startup-html/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../startup-html/**/*.html", "../startup-html/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
