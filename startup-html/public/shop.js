63716b03167d7d68114b73083d70ba7cc554a1fd
Cooper Motyer <cjmotyer@gmail.com>
2024-03-07 22:52:53 -0700

    updated shop page to display products from https://fakestoreapi.com

--- startup-html/public/shop.js
+++ startup-html/public/shop.js
@@ -27,7 +27,7 @@ function onStart() {
     }
 
     addEvents()
-    filterProducts()
+    getProducts()
 
 }
 
@@ -40,46 +40,44 @@ function addEvents() {
 }
 
 // database call goes here
-let products = [
-    new Product('Shirt1', 'shirt', '1', 'nike', '', '$12', '//img.ltwebstatic.com/images3_pi/2021/12/10/16391012757378a5e668f4378f9907939fab3eb501_thumbnail_900x.jpg'),
-    new Product('Shirt2', 'shirt', '2', 'nike', '', '$32', '//img.ltwebstatic.com/images3_pi/2021/12/10/16391012757378a5e668f4378f9907939fab3eb501_thumbnail_900x.jpg'),
-    new Product('Pants1', 'pants', '1', 'adidas', '', '$127', 'images/kingslandlogo_wordmark-blue.png'),
-    new Product('Pants2', 'pants', '2', 'adidas', '', '$2', 'images/kingslandlogo_wordmark-blue.png'),
-    new Product('Shoes1', 'shoe', '23', 'madeWell', '', '$645', 'images/kingslandlogo_wordmark-blue.png'),
-];
 
-let currentProduct = null;
 
-function filterProducts(filter) {
-    if (document.getElementById("product")) {
-        while (document.getElementById("product")){
-            document.getElementById("product").remove();
-        }
-        if (filter === 'brand') {
-            products.sort((p1, p2) => p1.brand - p2.brand);
-        } else if (filter === 'collection') {
-            products.sort((p1, p2) => p1.collection - p2.collection);
-        } else if (filter === 'style') {
-            products.sort((p1, p2) => p1.type - p2.type);
-        }
-        displayProducts();
-    }
-    else{ displayProducts(); }
-}
+let currentProduct;
+
 function displayProducts() {
     for (let product of products){
         document.getElementById("products").insertAdjacentHTML('beforeend',
-            '<div id="product" class="p-5 min-h-72 max-h-96 min-w-40 m-12 flex flex-col items-center justify-between">' +
-            '        <img id="product-img" class="h-1/2 min-w-32 place-self-center border-2" width="32" src="' + product.image + '" alt="product-image">\n' +
-            '        <div id="product-name" class="text-xl">' + product.name + '</div>' +
-            '        <div id="product-price" class="">' + product.price + '</div>' +
-            '        <button id="notifications" type="button" onclick="notifications(\'' + product.name + '\')"><span class="material-symbols-outlined">notifications</span></button>' +
-            '        <button id="add-to-cart-button" type="button" class="w-full h-1/5 bg-blue-400 px-4 py-2 text-sm hover:bg-blue-500 font-medium hover:font-bold hover:transition" onmouseenter="currentProduct = ' + product + '">ADD TO CART</button>\n' +
+            '<div id="product" class="p-5 h-80 w-60 m-10 flex flex-col items-center justify-between items-center text-center">' +
+            '        <img id="product-img" class="w-32 h-1/2" src="' + product.image + '" alt="product-image">\n' +
+            '        <div id="product-name" class="text-xl">' + product.title + '</div>' +
+            '        <div id="product-price" class="">$' + product.price + '</div>' +
+            '        <button id="notifications" type="button" onclick="notifications(\'' + product.title + '\')"><span class="material-symbols-outlined">notifications</span></button>' +
+            '        <button id="add-to-cart-button" type="button" class="w-full h-16 bg-blue-400 px-4 py-2 text-sm hover:bg-blue-500 font-medium hover:font-bold hover:transition" onmouseenter="currentProduct = ' + product + '">ADD TO CART</button>\n' +
             '     </div>');
     }
 }
 
 function notifications(productName) {
-    alert("You will now receive updates for " + productName + "!");
+    alert("You will now receive updates for " + productName + "!")
 
 }
+
+
+// Api call
+
+const fakeStoreApiUrl = 'https://fakestoreapi.com/products'
+
+let products;
+async function getProducts() {
+
+    fetch(fakeStoreApiUrl)
+        .then(res=>res.json())
+        .then(json=> {
+            console.log(json)
+            products = json
+            displayProducts()
+        });
+
+
+}
+
