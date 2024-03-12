93743c6d398d211247175ac67ab0abc85a17bdac
Cooper Motyer <cjmotyer@gmail.com>
2024-03-11 18:20:05 -0600

    added cart get, post, and delete functionality to service

--- startup-html/index.js
+++ startup-html/index.js
@@ -12,9 +12,46 @@ app.use(express.static('public'));
 
 // Router for service endpoints
 var apiRouter = express.Router();
-app.use('api', apiRouter);
+app.use('/api', apiRouter);
 
+// Get cart
+apiRouter.get('/cartItems', (_req, res) => {
+    res.send(cartItems);
+});
 
-app.listen(port, function () {
+apiRouter.post('/cartItem', (req, res) => {
+    cartItems = addCartItem(req.body, cartItems);
+    res.send(cartItems);
+});
+
+apiRouter.delete('/deleteCartItem', (req, res) => {
+    cartItems = deleteCartItem(req.body, cartItems);
+    res.send(cartItems);
+})
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+// updateCartItems updates cartItems with a new cart item
+// cart items saved in memory and disappear whenever the service is restarted.
+let cartItems = [];
+function addCartItem(newItem, cartItems) {
+    cartItems.push(newItem);
+    return cartItems;
+}
+
+function deleteCartItem(newItem, cartItems) {
+    let found = false;
+    for (const [i, item] of cartItems.entries()) {
+        if (item.values === newItem.values) {
+            found = true;
+            cartItems.splice(i, 1);
+            return cartItems;
+        }
+    }
+}
