8c7b038b7a5eaf559c6b808b4e8250ecea86f970
Cooper Motyer <cjmotyer@gmail.com>
2024-03-11 23:47:55 -0600

    added/fixed cart api, formatted display to show items saved in memory

--- startup-html/public/cart.js
+++ startup-html/public/cart.js
@@ -1,10 +1,57 @@
+window.addEventListener("load", onStart)
+let loggedIn = false;
+
+let cartItems;
+
 function onStart() {
-    let loggedIn = false;
 
     if (localStorage.getItem("userName")) {
         loggedIn = true;
         document.getElementById("cartUser").innerHTML = localStorage.getItem("userName");
     }
+    getCart();
 }
 
-window.addEventListener("load", onStart)
+async function getCart() {
+    try {
+        const response = await fetch('/api/cartItems');
+        if (!response.ok){
+            console.error('Failed to fetch products');
+        }
+        cartItems = await response.json();
+        if (cartItems.length > 0) {
+            console.log(cartItems)
+            document.getElementById("noItemsMessage").hidden = true;
+            loadCart(cartItems);
+            setSubtotal();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function loadCart(cartItems) {
+    document.getElementById("cartTitle").innerText = `Cart (${cartItems.length})`
+
+    let cartDiv = document.getElementById('cart');
+    for (const product of cartItems){
+        cartDiv.insertAdjacentHTML('beforeend',
+            '<div id="product" class=" min-w-40 m-5 flex flex-row">' +
+            '       <img id="product-img" class="h-32 min-w-32 border-2" width="32" src="' + product.image + '" alt="product-image">' +
+            '           <div class="flex flex-col px-4">' +
+            '              <div id="product-name" class="text-xl pb-4">' + product.title + '</div>' +
+            '              <div id="product-price" class="">$' + product.price + '</div>' +
+            '           </div>' +
+            '     </div>' +
+            '     <hr />'
+            )
+    }
+}
+
+function setSubtotal() {
+    let subtotal = 0;
+    for (let item of cartItems) {
+        subtotal += item.price;
+    }
+    document.getElementById("subtotal").innerText = "Subtotal: $" + subtotal.toString();
+}
