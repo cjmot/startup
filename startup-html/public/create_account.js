1cd3e0ee01874d1505119dcb3ef505cddbd2cd2a
Cooper Motyer <cjmotyer@gmail.com>
2024-03-22 22:31:45 -0600

    updated create_account.js to include database calls

--- startup-html/public/create_account.js
+++ startup-html/public/create_account.js
@@ -1,17 +1,29 @@
-function create_account() {
+async function create_account() {
     localStorage.clear();
-    const createName = document.getElementById("createName").value;
-    const createEmail = document.getElementById("createEmail").value;
-    const createPassword = document.getElementById("createPassword").value;
+    const userName = document.getElementById("createName").value;
+    const email = document.getElementById("createEmail").value;
+    const password = document.getElementById("createPassword").value;
     const confPassword = document.getElementById("passwordConfirmation").value;
-
-    if (confPassword !== createPassword) {
+    if (confPassword !== password) {
         alert("Passwords must match");
     }
     else {
-        alert("Account created")
-        localStorage.setItem("userName", createName);
-        localStorage.setItem("email", createEmail);
-        localStorage.setItem("password", createPassword);
+        const response = await fetch('api/auth/create', {
+            method: 'post',
+            body: JSON.stringify({email: userName, password: password}),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+
+        if (response.ok) {
+            alert("Account created");
+            localStorage.setItem("userName", userName);
+            localStorage.setItem("email", email);
+            localStorage.setItem("password", password);
+        } else {
+            const body = await response.json();
+            alert(body.msg)
+        }
     }
 }
