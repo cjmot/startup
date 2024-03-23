2dbf9d469d9644b3b00b681c076fbf22fb2e1281
Cooper Motyer <cjmotyer@gmail.com>
2024-03-22 22:31:31 -0600

    updated login.js to include database calls

--- startup-html/public/login.js
+++ startup-html/public/login.js
@@ -1,13 +1,23 @@
 function login() {
-    const userName = document.getElementById("name").value;
-    const password = document.getElementById("password").value;
-    if (userName !== localStorage.getItem("userName")){
-        alert("Username not recognized");
-    }
-    else if (password !== localStorage.getItem("password")){
-        alert("Password not recognized");
-    }
-    else {
+    loginUser()
+}
+
+async function loginUser() {
+    const endpoint = '/api/auth/create';
+    const userName = document.getElementById("name")?.value;
+    const password = document.getElementById("password")?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {'Content-type': 'application/json; charset=UTF-8',},
+    });
+
+    if (response.ok) {
+        localStorage.setItem('username', userName);
         window.location.href = "shop.html";
+    } else {
+        const body = await response.json();
+        alert(body.msg);
     }
 }
+
